d97b456f37805493b4ce7d6bbd29729af3500a2d
Gabby <gabbie10112005@gmail.com>
2024-11-12 14:31:18 -0600

    updates

--- HelloWorld_Miller_Gabriella/Program.cs
+++ HelloWorld_Miller_Gabriella/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("User! We're so happy to have you! What's your name? :D");
+//this line of code asks the user for their name
+string firstName = Console.ReadLine();
+//this line of code is where the user inputs their first name
+Console.WriteLine("Your name is really pretty! What's your last name, User?");
+//this line of code asks for the user's last name
+string lastName = Console.ReadLine();
+//this line of code is where the user inputs their last name
+
+
+Console.WriteLine($"Hello, {firstName} {lastName}! I really like your name! It's unique!");
+//this line of code is where the users first and last name is stated
